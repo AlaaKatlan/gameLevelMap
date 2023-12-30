223ca16d544f6a57c986098b518fbb5624a91429
Alaa Katlan <alaakatlan@Alaas-MacBook-Air.local>
2023-12-30 09:29:24 +0400

    change to stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:game_levels_scrolling_map/game_levels_scrolling_map.dart';
-import 'package:game_levels_scrolling_map/model/point_model.dart';
 import 'package:lottie/lottie.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-List<PointModel> points = [];
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MapVerticalExample(),
-      //home: MapVerticalExample(),
+      home: GameLevel(),
     );
   }
 }
 
-class MapVerticalExample extends StatefulWidget {
-  const MapVerticalExample({Key? key}) : super(key: key);
+class GameLevel extends StatelessWidget {
+  GameLevel({super.key});
+  final GlobalKey myKey = GlobalKey();
 
-  @override
-  State<MapVerticalExample> createState() => _MapVerticalExampleState();
-}
-
-class _MapVerticalExampleState extends State<MapVerticalExample> {
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: SizedBox(
-          child: GameLevelsScrollingMap.scrollable(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        imageHeight: MediaQuery.of(context).size.height,
-        imageWidth: MediaQuery.of(context).size.width,
-        imageUrl: "assets/drawable/gameleve.jpg",
-        direction: Axis.vertical,
-        reverseScrolling: true,
-        pointsPositionDeltaX: 40,
-        pointsPositionDeltaY: 30,
-        svgUrl: 'assets/svg/ponit-01.svg',
-        points: points,
-      )), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  @override
-  void initState() {
-    fillTestData();
-  }
-
-  List<PointModel> points = [];
-
-  void fillTestData() {
-    for (int i = 0; i < 30; i++) {
-      points.add(PointModel(100, testWidget(i)));
-    }
-  }
-
-  Widget testWidget(int order) {
-    return InkWell(
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Lottie.asset('assets/drawable/LottieLogo2.json', width: 50),
-          Text("$order",
-              style: const TextStyle(color: Colors.white, fontSize: 15))
-        ],
-      ),
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              content: Text("Point $order"),
-              actions: <Widget>[
-                ElevatedButton(
-                  child: const Text("OK"),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
+      body: Container(
+        width: screenSize.width,
+        height: screenSize.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/drawable/mem-bg.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              fit: StackFit.loose,
+              children: [
+                Container(
+                  color: Colors.red,
+                  key: myKey,
+                  child: Image.asset('assets/drawable/island.png'),
+                ),
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  child: Lottie.asset('assets/drawable/LottieLogo1.json',
+                      width: 100, fit: BoxFit.contain),
+                ),
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: Lottie.asset('assets/drawable/LottieLogo1.json',
+                      width: 100, fit: BoxFit.contain),
                 ),
               ],
-            );
-          },
-        );
-      },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
